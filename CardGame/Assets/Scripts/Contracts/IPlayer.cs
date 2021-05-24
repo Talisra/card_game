50393ad37562109c1a1f3b5ff04913a1813d6abe
Amir Solnik <Amir.Solnik@gmail.com>
2021-05-25 01:09:29 +0300

    added all interfaces as written on the board
    added all of the interface agreed on the blastoff meeting

---
+++ CardGame/Assets/Scripts/Contracts/IPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Assets.Scripts.Contracts
+{
+    public interface IPlayer
+    {
+        #region Props
+        ICastle Castle { get; set; }
+        List<ICard> Cards { get; set; }
+        int Resources { get; set; }
+        #endregion
+
+        #region Functions
+
+        #endregion
+    }
+}
