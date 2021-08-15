220ef12964f67641aa80a06cec517da0693ee779
Amir s <53918252+AmirSoln@users.noreply.github.com>
2021-08-15 19:59:51 +0300

    Dev turn mgr (#3)
    * scripts built
    
    * bar or clock are now functional and return to initial state after turn ends
    
    * added turn counter and round counter gui functionality

---
+++ CardGame/Assets/Scripts/TimeUi.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeUi : MonoBehaviour
+{
+    #region Fields
+    public string clockFormat = "{0:2D}:{1:2D}";
+    public Image uiTimerBarShaped;
+    public Text uiTimerClockShaped;
+
+    private float _maxTurnTime;
+    private Dictionary<CounterType, Action<float>> counterTypeToUpdateFunction = new Dictionary<CounterType, Action<float>>();
+        
+    #endregion
+
+    private void Start()
+    {
+        counterTypeToUpdateFunction.Add(CounterType.Bar, UpdateTimeLeftOfProgressBar);
+        counterTypeToUpdateFunction.Add(CounterType.Clock, UpdateTimeLeftOfTextField);
+    }
+
+    #region Public Functions
+
+    public void ConfigureTimerVisuals(CounterType counterType, float turnTime)
+    {
+        _maxTurnTime = turnTime;
+        if (counterType == CounterType.Bar)
+        {
+            uiTimerBarShaped.gameObject.SetActive(true);
+            uiTimerBarShaped.fillAmount = 1;
+        }
+        else
+        {
+            uiTimerClockShaped.gameObject.SetActive(true);
+            uiTimerClockShaped.text = string.Format(clockFormat, turnTime, 0);
+        }
+    }
+
+    public void UpdateVisuals(CounterType counterType, float remainingTime)
+    {
+        counterTypeToUpdateFunction[counterType](remainingTime);
+    }
+    #endregion
+
+    #region Private Functions
+    private void UpdateTimeLeftOfProgressBar(float remainingTime)
+    {
+        uiTimerBarShaped.fillAmount = remainingTime / _maxTurnTime;
+    }
+
+    private void UpdateTimeLeftOfTextField(float remainingTime)
+    {
+        var remainingTimeAsParts = remainingTime.ToString("0.00").Split('.');
+        uiTimerClockShaped.text = string.Format(clockFormat, remainingTimeAsParts[0], remainingTimeAsParts[1]);
+    }
+    #endregion
+
+}
---
+++ CardGame/Assets/Scripts/TurnMgr.cs
@@ -0,0 +1,74 @@
+﻿using Assets.Scripts.Contracts;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum CounterType { Bar, Clock }
+public class TurnMgr : MonoBehaviour
+{
+    #region Fields
+    public float turnTime;
+    public Queue<IPlayer> players;
+
+    private float timeLeft;
+    private int turnCounter;
+    private int roundCounter;
+    #endregion
+
+    #region GuiRelated
+    const string RoundCounterPrefix = "Round ";
+    const string TurnCounterPrefix = "Turn ";
+    public TimeUi timeUi;
+    public Text roundCounterTxt;
+    public Text turnCounterTxt;
+    public CounterType counterType;
+    #endregion
+
+    #region Public Core Functions
+    public void SwitchTurns()
+    {
+        //players.Enqueue(players.Dequeue()); relvent when players will be created
+        
+    }
+
+    public void EndTurn()
+    {
+        SwitchTurns();
+        InitTimerValues();
+    }
+    #endregion
+
+    #region Unity Functions
+    void Start()
+    {
+        InitTimerValues();
+        timeUi.ConfigureTimerVisuals(counterType, turnTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        timeUi.UpdateVisuals(counterType, timeLeft);
+        if (timeLeft <= 0)
+        {
+            EndTurn();
+        }
+    }
+    #endregion
+
+    #region Private Functions
+    private void InitTimerValues()
+    {
+        timeLeft = turnTime;
+        if (turnCounter % /*players.Count*/4 == 0)
+        {
+            roundCounter++;
+        }
+        turnCounter++;
+        roundCounterTxt.text = RoundCounterPrefix + roundCounter;
+        turnCounterTxt.text = TurnCounterPrefix + turnCounter;
+    }
+    #endregion
+
+}
