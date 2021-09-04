afc976704d0e62e926cc9b360f5cbef4e3c33ba7
Amir Solnik <Amir.Solnik@gmail.com>
2021-09-04 20:01:01 +0300

    added deck and deckMgr classes

---
+++ CardGame/Assets/Scripts/Deck.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deck : MonoBehaviour
+{
+    #region Properties
+    public List<BaseCard> currentGameCards;
+    #endregion
+
+
+    #region Public Functions
+    public void Draw()
+    {
+        Debug.Log("Draw is not yet implemented");
+    }
+
+    public void AddCardToDeck(BaseCard cardToAdd)
+    {
+        if(cardToAdd == null)
+        {
+            Debug.Log("Card cannot be null");
+            return;
+        }
+        currentGameCards.Add(cardToAdd);
+    }
+    #endregion
+
+    #region UnityFunctions
+    void Start()
+    {
+        currentGameCards = new List<BaseCard>();
+    }
+
+    void Update()
+    {
+
+    }
+    #endregion
+}
---
+++ CardGame/Assets/Scripts/DeckMgr.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeckMgr : MonoBehaviour
+{
+
+    #region Properties
+    public List<BaseCard> allGameCard;
+    public List<BaseCard> playerPreferedCards;
+    public Deck mainDeck;
+    public int deckSize;
+    #endregion
+
+
+    #region Public Functions
+    public void CreateDeck()
+    {
+        Debug.Log("CreateDeck is not yet implemented");
+    }
+
+    public void LoadCards()
+    {
+        Debug.Log("LoadCards is not yet implemented");
+    }
+    #endregion
+
+    #region UnityFunctions
+    void Start()
+    {
+        LoadCards();
+        CreateDeck();
+    }
+
+    void Update()
+    {
+
+    }
+    #endregion
+}
